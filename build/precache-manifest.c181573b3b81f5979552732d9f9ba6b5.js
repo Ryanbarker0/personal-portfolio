1245416a388ee164a8e45c5eb67a4f1e3a79d98e
Ryan <ryangeorgebarker0@gmail.com>
2019-01-24 21:29:24 +0000

    heroku

---
+++ build/precache-manifest.c181573b3b81f5979552732d9f9ba6b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "60244475a4478e5cebfe",
+    "url": "/static/js/main.60244475.chunk.js"
+  },
+  {
+    "revision": "40152ab8e98fac183506",
+    "url": "/static/js/1.40152ab8.chunk.js"
+  },
+  {
+    "revision": "60244475a4478e5cebfe",
+    "url": "/static/css/main.f7a8522b.chunk.css"
+  },
+  {
+    "revision": "fb564edf3e110301d9243f53a73bcead",
+    "url": "/index.html"
+  }
+];
